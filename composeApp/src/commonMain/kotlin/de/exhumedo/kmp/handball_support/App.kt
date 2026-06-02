2a7da6543e618ee02794a1d7ead2c5edcbbb99dd
Bjoern von der Linde <b.vonderlinde@reply.de>
2026-06-02 20:22:34 +0200

    refactor(compose): add `withFrameNanos` for frame-aware initialization in `App`

--- composeApp/src/commonMain/kotlin/de/exhumedo/kmp/handball_support/App.kt
+++ composeApp/src/commonMain/kotlin/de/exhumedo/kmp/handball_support/App.kt
@@ -12,6 +12,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.withFrameNanos
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavController
@@ -75,6 +76,7 @@ fun App() {
     }
 
     LaunchedEffect(navController) {
+        withFrameNanos { }
         applyInitialDeepLink(presenter, navController)
     }
 
